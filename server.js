d87cb2d397b998b76a18804a6de83106721ec40a
Gaurav Sharma <gaurav.sharma@zenways.io>
2019-05-24 16:35:23 +0530

    comment added

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 // set the view engine to ejs
+// https://stackoverflow.com/questions/15063262/is-there-any-way-to-use-multiple-view-engines-with-express-node-js
 app.set('view engine', 'ejs');
 
 // use res.render to load up an ejs view file
